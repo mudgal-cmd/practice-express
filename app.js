d7c3c7088a4c78a1253093f9d214cd3b9c50fab4
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-01-04 14:24:49 -0600

    Added navbar-app

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
+const {readFileSync} = require('fs');
+
+const homepage = readFileSync('./index.html', 'utf8');
+
 const server = http.createServer((req, res)=>{
   if(req.url === '/'){
     res.writeHead(200, {'content-type':'text/html'});
-    res.write('<h1>Request successfull</h2>');
+    // res.write('<h1>Request successfull</h2>');
+    res.write(homepage);
+    console.log('New request');
+    
     res.end();
   }
   else{
@@ -12,4 +19,6 @@ const server = http.createServer((req, res)=>{
   }
 });
 
-server.listen(5000);
+server.listen(5000, ()=>{
+  console.log('Server Listening at port 5000!!!');
+});
