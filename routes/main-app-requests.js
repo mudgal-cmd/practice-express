3c08a2d7f74dded16835c4a3fdbf7d789b2c79e4
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-01-24 18:15:59 -0600

    Created controllers for the express routes

--- routes/main-app-requests.js
+++ routes/main-app-requests.js
@@ -4,11 +4,7 @@ const router = express.Router();
 
 const products = require('../fetch-Data/fetch-products');
 
-// console.log(products());
-
 const people = require('../fetch-Data/fetch-users');
-
-// const app = express();
 
 // app.use(express.json()); //express.json() middleware is needed in app.js not here.
 
@@ -26,5 +22,46 @@ router.post('/addUser', (req, res)=>{
   res.status(200).json(people());
 
 });
+
+router.delete('/deleteUserById/:userId', (req, res)=>{
+
+  const {userId} = req.params;
+
+  people().map(user => {
+    
+    if(user.id === Number(userId)){
+      const deletedUser = user;
+
+      people().splice(people().indexOf(deletedUser), 1);
+      return res.status(200).json({success:true, data:people()});
+    }
+  });
+
+  res.status(404).json({success: false, data: `Could not find the user with id - ${userId}`});
+
+});
+
+router.put('/modifyUserById/:userId', (req,res)=>{
+
+  const {userId} = req.params;
+
+  const {name} = req.body;
+
+  const updatedUser = people().filter(user => user.id === Number(userId));
+
+  people().map(user=>{
+
+    if(user.id === Number(userId)){
+      user.name = name;
+      return res.status(200).json({success: true, data: people()});
+    }
+
+  }); 
+  
+  return res.status(404).json({success: false, msg: `Cannot update the user with ID: ${userId}`});
+
+});
+
+
 console.log(people());
 module.exports = router;
