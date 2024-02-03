eaefa5958c3f724e6a0f8d62505596dd7cc28e85
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-02-02 23:58:52 -0600

    Created cookie and parsed using the MW cookie-parser

--- express-cookie.js
+++ express-cookie.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const data = require('./constant');
 
-const session = require('express-session');
+// const session = require('express-session');
+
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 const axios = require('axios');
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 async function fetchCart(){
     
@@ -19,3 +23,20 @@ async function fetchCart(){
   return cartData;
 
 }
+
+app.get('/api/login', (req, res)=>{
+  res.cookie('loginCookie', 'authorized', {expires: new Date(Date.now()+90000)});
+  res.status(200).send('Authorized');
+});
+
+app.get('/api/getCart', (req, res)=>{
+  console.log(req.cookies);
+  if(req.cookies && req.cookies.loginCookie === 'authorized'){
+    
+  }
+});
+
+app.listen(5000, ()=>{
+  console.log('Server is listening on port 5000...');
+});
+
