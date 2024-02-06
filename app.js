843ec2623fc517ab227d875093fc031d661a97c4
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-02-05 22:15:52 -0600

    Implemented basic passport.js with local-strategy

--- app.js
+++ app.js
@@ -1,83 +1,38 @@
 const express = require('express');
 
-const data = require('./constant');
+const passport = require('passport');
 
 const session = require('express-session');
 
-const app = express();
+const localStrategy = require('./passport-strategies/local-strategy');
 
-const axios = require('axios');
+const app = express();
 
 app.use(express.json());
 
-async function fetchCart(){
-    
-  const response = await axios.get('https://fakestoreapi.com/carts');
+passport.use(localStrategy);
 
-  const cartData = response.data;
-
-  return cartData;
-
-}
-
-//To selectively use a middleware, just provide the endpoint before the middleware is invoked, eg: '/api/auth/status', here before session().
 app.use(session({
-  secret: 'session-secret-key',
-  saveUninitialized: false,
+  
+  secret: 'my-secret',
   resave: false,
+  saveUninitialized: false,
   cookie:{
-    maxAge: 90000
+    maxAge: 60000*60
   }
+
 }));
 
-app.get('/api/login', (req, res)=>{
-  
-  res.status(200).send(data);
+app.use(passport.initialize());
+app.use(passport.session()); // This will take care of attaching a dynamic user property ('user') to the req object to let us know who the actual user is that's making the request.
 
-});
 
-app.post('/api/auth', (req, res)=>{
 
-  const {username, password} = req.body;
+app.post("/api/login", passport.authenticate('local'), (req, res)=>{
+  console.log('Hi from login');
 
-  const findUser = data.find(user => user.username === username);
-
-  if(findUser && findUser.password === password){
-    req.session.user = findUser;
-    return res.status(200).send('Authentication successfull');
-  }
-  return res.status(401).send('Authentication Failed!! Please provide valid credentials');
-
-});
-
-app.get('/api/auth/status', (req, res)=>{
-
-  console.log(req.session);
-
-  return req.session.user? res.status(200).send(req.session.user) : res.send("Not authenticated");
-
-});
-
-app.post('/api/cart/addToCart', (req, res)=>{
-  // 
-  const cartData = fetchCart();
-
-  const {body} = req;
-
-  cartData.then(cart => {
-    
-    // console.log(cart);
-
-    // console.log(body);
-    if(req.session.user){
-      
-    }
-
-    cart.push(body);
-
-    res.status(200).json({success:true, data: cart});
-
-  }).catch(err=> console.log(err));
+  res.sendStatus(200);
+//We can specifiy different strategies inside the passport.authenticate method, like 'google', 'github', etc.
 
 });
 
---
+++ passport-strategies/local-strategy.js
@@ -0,0 +1,57 @@
+const passport = require('passport');
+
+const Strategy = require('passport-local');
+
+const userData = require('../constant');
+
+passport.serializeUser((user, done)=>{
+  console.log('Serializer function');
+  console.log(user);
+  done(null, user.id);
+  
+});
+
+passport.deserializeUser((id, done)=>{
+  
+  console.log('Deserializer function');
+  
+  try{
+
+    const findUser = userData.find(userEntry => userEntry.id === id);
+
+    if(!findUser) throw new Error("User not found");
+
+    done(null, findUser);
+
+
+  }
+  catch(err){
+    done(err, null);
+  }
+});
+
+const strategyVerifyFunc = (username, password, done)=>{
+  try{
+    
+    console.log(`Username: ${username}`);
+    console.log(`Password: ${password}`);
+
+    const findUser = userData.find(user => username === user.username);
+
+    if(!findUser){throw new Error('Error: User not found');}
+
+    // console.log(findUser);
+
+    if(password!==findUser.password){throw new Error('Error: Please provide the correct credentials');}
+
+    done(null, findUser); //null here is for the error.
+
+  }
+  catch(err){
+    done(err, null); //null here is for the user data, because of error no user will be returned.
+  } 
+}
+
+const localStrategy = new Strategy(strategyVerifyFunc);
+
+module.exports = localStrategy;
